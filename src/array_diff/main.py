2da6f3c3f5eafe50c8151bcce154717a2171a2bb
tope96 <pietrzaktomek@hotmail.com>
2021-03-07 20:57:19 +0100

    Array diff alternative

--- src/array_diff/main.py
+++ src/array_diff/main.py
@@ -8,5 +8,10 @@ def array_diff(a, b):
     return list
 
 
+def array_diff_alt(a, b):
+    # alternative solution:
+    return [x for x in a if x not in b]
+
+
 print(array_diff([-5, -11, 15, -17, -1, -10, 13, -2, 20, 8, -8, 0, -6, 20, 10, 17, 1], [0, -6, -15, -15, -13, -13]))
-print(array_diff([1, 2, 2], [1, 2]))
+print(array_diff_alt([1, 2, 2], [1]))
